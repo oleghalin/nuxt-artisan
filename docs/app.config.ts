0543a0642aa4c6e787b225fa87ff3dfe6e46f3dc
u <oleghalinua@gmail.com>
2023-03-06 16:59:40 +0100

    fix: docs submodules

---
+++ docs/app.config.ts
@@ -0,0 +1,26 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Nuxt Artisan',
+    description: 'The best place to start your documentation.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      twitter: 'khalinol',
+      github: 'oleghalin/nuxt-artisan'
+    },
+    github: {
+      repo: 'oleghalin/nuxt-artisan'
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: []
+    },
+    footer: {
+      iconLinks: []
+    }
+  }
+})
